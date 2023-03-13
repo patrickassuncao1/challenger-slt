16175b25a18dc87bf84f737fb95b3ade3aa65f3f
patrickassuncao1 <patrickassuncao1@gmail.com>
2023-03-13 14:53:48 -0300

    feat: connection database

---
+++ app/database/Database.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\database;
+
+use PDO;
+
+class Database
+{
+    private $table;
+    private  $connection;
+
+    public function __construct($table = null)
+    {
+
+        $this->table = $table;
+        $this->setConnection();
+    }
+
+    private function setConnection()
+    {
+        $HOST = getenv("DB_HOST");
+        $NAME = getenv("DB_DATABASE");
+        $USER = getenv("DB_USERNAME");
+        $PASSWORD = getenv("DB_PASSWORD");
+
+        try {
+            $this->connection = new PDO("mysql:host=" . $HOST . ";dbname=" . $NAME, $USER, $PASSWORD);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+            die("Error " . $e->getMessage());
+        }
+    }
+
+    public function execute($query, $params = [])
+    {
+
+        try {
+
+            $statement = $this->connection->prepare($query);
+            $statement->execute($params);
+
+            return $statement;
+        } catch (\PDOException $th) {
+            //throw $th;
+        }
+    }
+
+    public function insert(array $values) : int
+    {
+        $fields = array_keys($values);
+        $binds = array_pad([], count($fields), '?');
+
+        $arrayToStringFields =  implode(',', $fields);
+        $arrayToStringBinds =  implode(',', $binds);
+
+        $query = "INSERT INTO {$this->table} ($arrayToStringFields) VALUES ($arrayToStringBinds)";
+
+        $this->execute($query, array_values($values));
+
+        return $this->connection->lastInsertId();
+    }
+}
